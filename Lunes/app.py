4afdc9ca23722f4a31d844a5b6539e1a10c110e6
FICT1 <ficallejas@uamv.edu.ni>
2025-05-06 11:26:35 -0600

    Trabajo en Clase

---
+++ Lunes/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    msn = ""
+    for num in range(1, 11):
+            msn += str (num)
+    return render_template('index.html', msn=msn)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
